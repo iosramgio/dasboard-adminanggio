cb6bce8f8116a5e0b13e377b43755007e0f2d579
iosramgio <146721654+iosramgio@users.noreply.github.com>
2024-10-26 20:51:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// sidebar dropdown
+const allDropdown =document.querySelectorAll('#sidebar .side-dropdown');
+
+allDropdown.forEach(item => {
+    const a = item.parentElement.querySelector('a:first-child');
+    a.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        if(!this.classList.contains('active')){
+            allDropdown.forEach(i=>{
+                const aLink = i.parentElement.querySelector('a:first-child');
+
+                aLink.classList.remove('active');
+                i.classList.remove('show');
+            })
+        }
+
+        this.classList.toggle('active');
+        item.classList.toggle('show'); 
+    })
+})
+
+// profile dropdown
+const profile = document.querySelector('nav .profile');
+const imgProfile = profile.querySelector('img');
+const dropdownProfile = profile.querySelector('.profile-link');
+
+imgProfile.addEventListener('click', function () {
+	dropdownProfile.classList.toggle('show');
+})
